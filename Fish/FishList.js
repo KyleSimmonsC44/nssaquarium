c26822fddc043b00de7664ae1b756ee3deefb45d
KyleSimmonsC44 <darrensimmons92@gmail.com>
2020-10-09 13:55:08 -0500

    sorted fish by level of holiness because martin is taking drugs now

--- Fish/FishList.js
+++ Fish/FishList.js
@@ -1,23 +1,44 @@
 import {Fish} from "./Fish.js"
-import{ useFish } from "./FishDataProvider.js"
+import{ useFish, mostHolyFish, otherFish, soldier } from "./FishDataProvider.js"
 
+const buildFishContainerHTML = (arrayOfFish) =>{
+    
+    let fishHTMLRepresentations = ""
 
+    for (const fish of arrayOfFish) {
+
+        fishHTMLRepresentations += Fish(fish)
+
+    }
+    return fishHTMLRepresentations
+}
 
 export const FishList = () =>{
     const contentElement=document.querySelector(".containerLeft")
 
-    const fishes = useFish()
+    const holyFishes = mostHolyFish()
 
-    let fishHTMLRepresentations = ""
-    for (const fish of fishes) {
-        fishHTMLRepresentations += Fish(fish)
-    }
+    const holyFishHTML = buildFishContainerHTML(holyFishes)
+
+    const soldierFishes = soldier()
+
+    const soldierFishHTML = buildFishContainerHTML(soldierFishes)
+
+    const otherFishes = otherFish()
+
+    const otherFishesHTML = buildFishContainerHTML(otherFishes)
+
 
 
     contentElement.innerHTML +=`
     <h3>Meet the Fish!</h3>
     <section class="fishList">
-       ${fishHTMLRepresentations}
+    <p class="holyFish">Holy Fish</p>
+       ${holyFishHTML}
+    <p class="holyFish">Soldier Fish</p>
+       ${soldierFishHTML}
+    <p class="holyFish">Other Fish</p>
+       ${otherFishesHTML}
     </section>
     `
 }
